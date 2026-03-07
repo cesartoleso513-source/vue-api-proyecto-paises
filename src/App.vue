94839251e7bdd9edc267e9db8b24e08e5806d165
Cesar Yael <cesartoleso513@gmail.com>
2026-03-07 10:07:06 -0600

    segundo poryecto de la unidad 3

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { ref, onMounted, computed } from "vue"
+import CountryCard from "./components/CountryCard.vue"
+
+const paises = ref([])
+const cargando = ref(true)
+const busqueda = ref("")
+
+onMounted(async () => {
+  try {
+    const res = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags,capital,region,subregion,population,languages,currencies"
+    )
+    paises.value = await res.json()
+  } catch (error) {
+    console.log("Error al cargar países", error)
+  } finally {
+    cargando.value = false
+  }
+})
+
+const paisesFiltrados = computed(() => {
+  return paises.value.filter(p =>
+    p.name.common.toLowerCase().includes(busqueda.value.toLowerCase()) ||
+    (p.region && p.region.toLowerCase().includes(busqueda.value.toLowerCase()))
+  )
+})
+</script>
+
+<template>
+  <div class="container">
+
+    <h1>🌎 Explorador de Países</h1>
+
+    <input
+      v-model="busqueda"
+      placeholder="Buscar país..."
+      class="search"
+    />
+
+    <p v-if="cargando">⏳ Cargando países...</p>
+
+    <div v-else class="grid">
+      <CountryCard
+        v-for="pais in paisesFiltrados"
+        :key="pais.name.common"
+        :pais="pais"
+      />
+    </div>
+
+    <p v-if="!cargando && paisesFiltrados.length === 0">
+      No se encontraron países
+    </p>
+
+  </div>
+</template>
+
+<style>
+
+.container{
+  max-width:1200px;
+  margin:auto;
+  text-align:center;
+}
+
+.search{
+  padding:10px;
+  width:300px;
+  margin:20px;
+}
+
+.grid{
+  display:grid;
+  grid-template-columns:repeat(auto-fill,minmax(250px,1fr));
+  gap:20px;
+}
+
+</style>
---
+++ src/components/CountryCard.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    pais: Object
+  }
+}
+</script>
+
+<template>
+<div class="card">
+
+  <img :src="pais.flags?.png" alt="bandera">
+
+  <h2>{{ pais.name?.common }}</h2>
+
+  <p><b>Capital:</b> {{ pais.capital ? pais.capital[0] : "Sin capital" }}</p>
+
+  <p><b>Región:</b> {{ pais.region }}</p>
+
+  <p><b>Población:</b> {{ pais.population?.toLocaleString() }}</p>
+
+</div>
+</template>
+
+<style>
+.card{
+  border:1px solid #ddd;
+  border-radius:10px;
+  padding:15px;
+  background:white;
+  box-shadow:0 2px 5px rgba(0,0,0,0.1);
+}
+
+.card img{
+  width:100%;
+  height:140px;
+  object-fit:cover;
+  border-radius:6px;
+}
+</style>
